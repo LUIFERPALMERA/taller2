655a1b18e865dc802ba7f71fc688d118f20a96cc
unknown <lpalmera@unimagdalena.edu.co>
2019-07-22 16:18:27 -0500

    Commit inicial

---
+++ app/src/main/java/com/example/celulares/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.celulares;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.celulares.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
